cd57f818967653e948460bbd26cb1caba7e922f9
Ganesh Reddy Lakka <158273346+ganesnitw37@users.noreply.github.com>
2024-09-28 07:14:56 +0530

    Create sqlclass3.sql

---
+++ sqlclass3.sql
@@ -0,0 +1,43 @@
+--sub Query
+--1. select the visitor first name & last name who have spent greater than 2000 and also use alaising
+SELECT v.ff_name,v.ll_name
+FROM visitor v
+WHERE v.code IN(
+    SELECT t.code 
+    FROM transactions t
+    WHERE t.total_bill>1500
+);
+
+--2. Use Union ALL to select the visitor first name & last name who have spent greater than 2000
+SELECT ff_name,ll_name
+FROM visitor 
+UNION ALL
+SELECT v.ff_name,v.ll_name
+FROM visitor v
+WHERE v.code IN(
+    SELECT t.code 
+    FROM transactions t
+    WHERE t.total_bill>1500
+);
+
+SELECT ff_name,ll_name
+FROM visitor 
+UNION 
+SELECT v.ff_name,v.ll_name
+FROM visitor v
+WHERE v.code IN(
+    SELECT t.code 
+    FROM transactions t
+    WHERE t.total_bill>1500
+);
+
+SELECT ff_name,ll_name
+FROM visitor 
+INTERSECT
+SELECT v.ff_name,v.ll_name
+FROM visitor v
+WHERE v.code IN(
+    SELECT t.code 
+    FROM transactions t
+    WHERE t.total_bill>1500
+);
